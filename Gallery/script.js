b8b9cb69597645202131b879fd93685368254d3b
Nimira43 <n.rabone72@aol.com>
2025-08-09 08:44:14 +0100

    for loop - 2

--- Gallery/script.js
+++ Gallery/script.js
@@ -6,5 +6,12 @@ let boolVal = false
 
 for (let i = 0; i < imagesCount; i++) {
   const div = document.createElement('div')
-  div.classList.add('image-wrapper')
+  div.classList.add('img-wrapper')
+
+  const img = document.createElement('img')
+  img.classList.add('grid-img')
+
+  img.src = `images/image-${i + 1}.jpg`
+  div.append(img)
+  grid.append(div)
 }
